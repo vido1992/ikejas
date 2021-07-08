6c2c8a7a896491139aa8c679152af97a3916ad7d
Sebastian <sebas_noobii@hotmail.com>
2021-07-08 17:41:00 -0500

    fix

--- IKEJA/src/environments/environment.ts
+++ IKEJA/src/environments/environment.ts
@@ -3,7 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  keycloakConfig:{
+    "url": "http://localhost:8081/auth",
+      "realm": "IKEJAS",
+      "clientId": "ikejas"
+  }
 };
 
 /*
