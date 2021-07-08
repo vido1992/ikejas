4a9c94d2bc337743f173ac5b0daecd7829703e12
Sebastian <sebas_noobii@hotmail.com>
2021-07-07 21:05:24 -0500

    Configuraciones iniciales para keycloak

---
+++ IKEJA/src/environments/AppConfigType.ts
@@ -0,0 +1,11 @@
+export default class AppConfigType {
+    security: {
+        url: string,
+        realm: string,
+        clientId: string
+    };
+
+    CATALOGOS: {
+        CODIGO_TIPO_GENERO: number
+    };
+}
