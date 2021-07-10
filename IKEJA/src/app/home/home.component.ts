b9cfb6259c90e9703d584f6564fcee31111ab685
Sebastian <sebas_noobii@hotmail.com>
2021-07-09 21:36:16 -0500

    deleting information from home component

--- IKEJA/src/app/home/home.component.ts
+++ IKEJA/src/app/home/home.component.ts
@@ -8,28 +8,8 @@ import { KeycloakService } from 'keycloak-angular';
 })
 export class HomeComponent implements OnInit {
 
-  public userObject: any
-  public token: any
-  public refreshToken: any
-
-  constructor(protected readonly keycloak: KeycloakService) {
-    
-  }
   async ngOnInit(): Promise<void> {
-    this.userObject = await this.keycloak.loadUserProfile();
-    this.token = await this.keycloak.getToken()
-    this.refreshToken = this.keycloak.getKeycloakInstance().refreshToken
-  }
 
-  logout() {
-    this.keycloak.logout("http://localhost:9006/logout")
-  }
-
-  async update() {
-    await this.keycloak.updateToken(-1).then(async () => {
-      this.token = await this.keycloak.getToken()
-    })
-    this.refreshToken = this.keycloak.getKeycloakInstance().refreshToken
   }
 
 }
